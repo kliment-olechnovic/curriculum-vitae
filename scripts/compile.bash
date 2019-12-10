6c82539785e34f0143a5b7b6b67593226c289154
kliment-olechnovic <kliment.olechnovic@gmail.com>
2019-12-10 19:36:02 +0200

    Added conversion to pdf rom html.

--- scripts/compile.bash
+++ scripts/compile.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm ./index.html
+rm ./index.html ./index.pdf
 
 pandoc ./index.markdown -f markdown -t html -s -o ./index.html --css pandoc.css
 
+wkhtmltopdf ./index.html ./index.pdf > /dev/null
