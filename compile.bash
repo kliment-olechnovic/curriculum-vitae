14e736ee20ff4b28d7a8efd5e156b81ac0e977a0
kliment-olechnovic <kliment.olechnovic@gmail.com>
2021-03-15 16:07:59 +0200

    Added html head titles.

--- compile.bash
+++ compile.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname $0)"
+
 rm ./Kliment_Olechnovic_CV.html ./Kliment_Olechnovic_CV.pdf
 pandoc ./Kliment_Olechnovic_CV.markdown --css pandoc.css -f markdown -t html -s -o ./Kliment_Olechnovic_CV.html
 wkhtmltopdf -L 12mm -R 12mm -T 12mm -B 12mm ./Kliment_Olechnovic_CV.html ./Kliment_Olechnovic_CV.pdf > /dev/null
@@ -13,3 +15,11 @@ pandoc ./Kliment_Olechnovic_CV.markdown --css pandoc_with_toc.css -f markdown -t
 
 rm ./index_lt.html
 pandoc ./Kliment_Olechnovic_CV_lt.markdown --css pandoc_with_toc.css -f markdown -t html -s --toc --toc-depth=2 -o ./index_lt.html
+
+
+find ./ -mindepth 1 -maxdepth 1 -type f -name '*.html' \
+| while read -r pagefile
+do
+	cat "$pagefile" | sed 's|<title></title>|<title>Dr. Kliment Olechnovič - Curriculum Vitae</title>|' | sponge "$pagefile"
+done
+
