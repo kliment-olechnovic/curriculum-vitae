7b6fe97f373f904a1ca0353ed96ab41d1ef9b7ff
kliment-olechnovic <kliment.olechnovic@gmail.com>
2021-07-05 16:06:40 +0300

    Added compact version generation.

--- compile.bash
+++ compile.bash
@@ -10,6 +10,12 @@ rm ./Kliment_Olechnovic_CV_lt.html ./Kliment_Olechnovic_CV_lt.pdf
 pandoc ./Kliment_Olechnovic_CV_lt.markdown --css pandoc.css -f markdown -t html -s -o ./Kliment_Olechnovic_CV_lt.html
 wkhtmltopdf -L 12mm -R 12mm -T 12mm -B 12mm ./Kliment_Olechnovic_CV_lt.html ./Kliment_Olechnovic_CV_lt.pdf > /dev/null
 
+rm ./Kliment_Olechnovic_CV_compact.pdf
+cat ./Kliment_Olechnovic_CV.markdown | sed 's/  $//' > ./Kliment_Olechnovic_CV_compact.markdown
+pandoc ./Kliment_Olechnovic_CV_compact.markdown --css pandoc_compact.css -f markdown -t html -s -o ./Kliment_Olechnovic_CV_compact.html
+wkhtmltopdf -L 12mm -R 12mm -T 12mm -B 12mm ./Kliment_Olechnovic_CV_compact.html ./Kliment_Olechnovic_CV_compact.pdf > /dev/null
+rm ./Kliment_Olechnovic_CV_compact.markdown ./Kliment_Olechnovic_CV_compact.html
+
 rm ./index.html
 pandoc ./Kliment_Olechnovic_CV.markdown --css pandoc_with_toc.css -f markdown -t html -s --toc --toc-depth=2 -o ./index.html
 
